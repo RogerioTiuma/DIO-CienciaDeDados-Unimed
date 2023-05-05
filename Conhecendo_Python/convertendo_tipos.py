3a6cfd417b22e5d3d5945a869d959fbc0f41c269
RogerioTiuma <rogeriotiuma@hotmail.com>
2023-05-05 13:56:43 -0300

    finalização do módulo 1

---
+++ Conhecendo_Python/convertendo_tipos.py
@@ -0,0 +1,15 @@
+print(int(1.0))
+print(int(1.973458))
+print(int("15"))
+print(float("15.86"))
+
+print(float(100))
+
+valor = 10
+valor_str = str(valor)
+
+print(type(valor))
+print(type(valor_str))
+
+print(100/2)
+print(100//2)
